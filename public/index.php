34b92dfcf76ae312e8d5350a9d6697135f16a240
faishal firman hakim <isalredarmy1@gmail.com>
2021-01-24 17:03:57 +0700

    edit lagi

--- public/index.php
+++ public/index.php
@@ -46,10 +46,10 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// $kernel = $app->make(Kernel::class);
-//
-// $response = tap($kernel->handle(
-//     $request = Request::capture()
-// ))->send();
-//
-// $kernel->terminate($request, $response);
+$kernel = $app->make(Kernel::class);
+
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
+
+$kernel->terminate($request, $response);
