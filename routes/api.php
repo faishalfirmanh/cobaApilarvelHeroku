f19bd1a748fbc4938a89a54ba177fab5f2841025
faishal firman hakim <isalredarmy1@gmail.com>
2021-02-17 16:34:45 +0700

    new controller

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::resource('Places','CobaTanpaReload');
+
 // Route::post('Product','ProductController@store');
 Route::resource('Product', 'ProductController');
 // Route::get('ProductGet','ProductController@index');
 // Route::delete('ProductDel/{id}','ProductController@destroy');
-
 Route::post('Prod/{id}','ProductController@updateNew');
 
 Route::post('Kategori','ProductController@ByKategory');
